aaab8ec16964ed8e534b14838d52e101ce0078d5
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-06-14 17:46:54 -0400

    Completed up to item 39

--- _.js
+++ _.js
@@ -1,4 +1,5 @@
 // Object which will represent our library containing all the functionality we add to it.
+// my alternative solutions are commented with the comment characters on the first column
 const _ = {
     // receives a number and two limits, returns the closest value to number between those limits
     clamp(number, lower, upper) {
@@ -43,9 +44,10 @@ const _ = {
     pad(string, length) {
         // if string length is above target length the original string is returned
         if (string.length > length) return string;
-
-        Math.floor((length - string.length) / 2);
-        
+        const startPaddingLength = Math.floor((length - string.length) / 2);
+        const endPaddingLength = length - string.length - startPaddingLength;
+        const paddedString = ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength);
+        return paddedString;
         // alternative solution below (all other lines on the method must be commented)
         // we execute a while loop as long as desired length is above string length
 //        while (length > string.length) {
@@ -57,6 +59,88 @@ const _ = {
 //            string = ' ' + string;
 //        }
 //        return string;
+    },
+    has(object, key) {
+        const hasValue = (object[key] != undefined);
+        return hasValue;
+        // alternative solution below (all other lines on the method must be commented)
+        // if the key at object is undefinded false is returned
+//        if (object[key] === undefined) return false;
+        // true is returned by default
+//        return true;
+    },
+    invert(object) {
+        const invertedObject = {};
+        for (const key in object) {
+            const originalValue = object[key];
+            invertedObject[originalValue] = key;
+        }
+        return invertedObject;
+        // alternative solution below (all other lines on the method must be commented)
+        // temporal variable to store the inverted object
+//        const invertedObject = {};
+        // for ... in loop to cycle trough the object
+//        for (const element in object) {
+            // new object key - value stored
+//            invertedObject[object[element]] = element;
+//        }
+//        return invertedObject;
+    },
+    findKey(object, predicate) {
+//        for (const key in object) {
+//            const value = object[key];
+//            const predicateReturnValue = predicate(value);
+//            if (predicateReturnValue) return key;
+//        }
+//        return undefined;
+        // for ... in cycle trough every key
+        for (const key in object) {
+            // the predicate function 
+            if (predicate(object[key])) return key;
+        }
+        return undefined;
+    },
+    drop(array, number) {
+        if (number === undefined) number = 1;
+        const droppedArray = array.slice(number);
+        return droppedArray;
+        // alternative solution below (all other lines on the method must be commented)
+        // number is set to 1 as default (if it is not set on the method call)
+//        if (number === undefined) number = 1;
+        // the new array is returned with filtered elements
+//        return array.filter(element => {
+//            if (number <= 0) return true
+//            number--;
+//            return false;
+//        });
+    },
+    dropWhile(array, predicate) {
+        const dropNumber = array.findIndex((element, index) => {return !predicate(element, index, array)});
+        const droppedArray = this.drop(array, dropNumber);
+        return droppedArray;
+        // alternative solution below (all other lines on the method must be commented)
+        // auxiliary index value
+//        let i = 0;
+        // cycle through the array until the predicate returns false
+//        while (predicate(array[i], i, array)) {
+            // the index is incremented on every cycle
+//            i++;
+//        }
+        // an array is returned with the first "i" elements sliced
+//        return array.slice(i);
+    },
+    chunk(array, size) {
+        if (size === undefined) size = 1;
+        const chunkedArray = [];
+        while (array.length > 0) {
+            const chunk = [];
+            for (let i = 0 ; i < size ; i++) {
+                chunk[i] = array.shift();
+                if (array.length === 0) break;
+            }
+            chunkedArray.push(chunk);
+        }
+        return chunkedArray;
     }
 }
 
